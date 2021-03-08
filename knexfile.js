d331ad6b5a9ada17681e900183e2ab715ae9f4bb
Kat Leight <leightkt@gmail.com>
2021-03-08 10:11:07 -0700

    added user table migration

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgresql://localhost/voice2vibes'
+    connection: 'postgresql://localhost/voice2vibes_development'
   },
 
   staging: {
