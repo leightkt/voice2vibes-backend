18d7f975ed27afdc2ced08d39ac33abd9cdf65af
Kat Leight <leightkt@gmail.com>
2021-03-08 09:46:03 -0700

    set up express, nodemon, cors

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 9000
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,POST,PUT,PATCH,DELETE'
+}
+
+app.use(cors(corsOptions))
+
+app.listen(port, () => console.log(`running on ${port}`))
