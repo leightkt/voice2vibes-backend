b52685a3d70119df187229ab5255cd5f5e82f3cb
Kat Leight <leightkt@gmail.com>
2021-03-11 14:27:25 -0700

    add ssl to production

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,7 @@ module.exports = {
 
   production: {
     client: "pg",
+    ssl: { rejectUnauthorized: false },
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/migrations'
