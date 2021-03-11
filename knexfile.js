3958b9f88d97a0cd82260b1c005493cb7e65c05c
Kat Leight <leightkt@gmail.com>
2021-03-11 13:49:30 -0700

    update knex file

--- knexfile.js
+++ knexfile.js
@@ -11,8 +11,13 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL
+    host: "ec2-3-222-11-129.compute-1.amazonaws.com",
+    database: "dbjfv6jdkmfiqs",
+    user: "nlkzimsxtnwodh",
+    port: "5432",
+    password: "ab1453c651956f86341cfc13e8fc25d61894de57675c0ac4da4ca9a431ce1021",
+    dialect: 'postgres',
+    use_env_variable: DATABASE_URL
   }
 
 };
