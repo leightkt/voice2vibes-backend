f04bbd3576f8f34968786904d746c70f12a5c60b
Kat Leight <leightkt@gmail.com>
2021-03-11 14:22:56 -0700

    add pg.default to database

--- database/Database.js
+++ database/Database.js
@@ -1,5 +1,7 @@
 const config = require('../knexfile')[process.env.NODE_ENV || 'development']
 // const config = require('../knexfile').development
+const pg = require('pg')
+pg.defaults.ssl = true
 const knex = require('knex')
 const database = knex(config)
 
--- knexfile.js
+++ knexfile.js
@@ -11,18 +11,6 @@ module.exports = {
   },
 
   production: {
-    type: "postgres",
-    host: "ec2-3-222-11-129.compute-1.amazonaws.com",
-    port: "5432",
-    username: "nlkzimsxtnwodh",
-    password: "ab1453c651956f86341cfc13e8fc25d61894de57675c0ac4da4ca9a431ce1021",
-    database: "dbjfv6jdkmfiqs",
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      }
-    },
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
