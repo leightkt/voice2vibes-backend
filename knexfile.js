668f97edbd497214ab8b6e170160193a23c88d06
Kat Leight <leightkt@gmail.com>
2021-03-11 14:15:37 -0700

    update knexfile sith ssl

--- knexfile.js
+++ knexfile.js
@@ -18,6 +18,11 @@ module.exports = {
     password: "ab1453c651956f86341cfc13e8fc25d61894de57675c0ac4da4ca9a431ce1021",
     database: "dbjfv6jdkmfiqs",
     ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      }
+    },
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/migrations'
